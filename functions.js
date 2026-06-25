2f7b4161b1841a9e68b0116593b15c55baade5b6
walterlars <lars.michelbach@web.de>
2026-06-25 21:26:04 +0200

    Remove all repository content

--- functions.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.orgame').addEventListener('click', function() {
-        window.location.href = 'organizeme.html';
-    });
-});
